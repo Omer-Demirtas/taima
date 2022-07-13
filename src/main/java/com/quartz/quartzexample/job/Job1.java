a028bd64aef73d66ee27ba26b0f6d125e9cfca3a
OmerHalil <gamega55834@gmail.com>
2022-07-13 09:51:06 +0300

    Job 1 updated.

--- src/main/java/com/quartz/quartzexample/job/Job1.java
+++ src/main/java/com/quartz/quartzexample/job/Job1.java
@@ -1,13 +1,18 @@
 package com.quartz.quartzexample.job;
 
+import lombok.extern.log4j.Log4j2;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
+import java.time.LocalDateTime;
+
+@Log4j2
 public class Job1 extends QuartzJobBean
 {
     @Override
-    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException
+    {
+        log.info("Job 1 running now {}", LocalDateTime.now());
     }
 }
