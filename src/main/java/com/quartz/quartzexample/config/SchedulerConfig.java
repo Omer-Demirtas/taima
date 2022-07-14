97d08ff346a5b1c99f06ac338e69b1cba2e6e73f
OmerHalil <gamega55834@gmail.com>
2022-07-14 10:37:45 +0300

    config update

--- src/main/java/com/quartz/quartzexample/config/SchedulerConfig.java
+++ src/main/java/com/quartz/quartzexample/config/SchedulerConfig.java
@@ -37,7 +37,7 @@ public class SchedulerConfig
         //You may use the value “AUTO” as the instanceId if you wish the Id to be generated for you.
         //Or the value “SYS_PROP” if you want the value to come from the system property “org.quartz.scheduler.instanceId”.
         properties.setProperty("org.quartz.scheduler.instanceId", "AUTO");
-        properties.setProperty("org.quartz.scheduler.instanceName", "asdf");
+        properties.setProperty("org.quartz.scheduler.instanceName", "Quartz-Scheduler");
 
         //JobStoreTX manages all transactions itself by calling commit() (or rollback()) on the database connection after every action
         //(such as the addition of a job). JDBCJobStore is appropriate if you are using Quartz in a stand-alone application,
