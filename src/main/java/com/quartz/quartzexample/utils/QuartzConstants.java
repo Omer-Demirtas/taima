306339c143b7adf0251f788339b47ff451cda188
OmerHalil <gamega55834@gmail.com>
2022-07-14 10:36:55 +0300

    Remove unnecessary Job

--- src/main/java/com/quartz/quartzexample/utils/QuartzConstants.java
+++ src/main/java/com/quartz/quartzexample/utils/QuartzConstants.java
@@ -2,7 +2,6 @@ package com.quartz.quartzexample.utils;
 
 import com.quartz.quartzexample.dto.QuartzJobDTO;
 import com.quartz.quartzexample.dto.QuartzTriggerDTO;
-import com.quartz.quartzexample.job.Job1;
 import com.quartz.quartzexample.service.impl.Job1Service;
 
 import java.util.HashMap;
@@ -12,22 +11,6 @@ public class QuartzConstants
 {
     public static final Map<String, QuartzJobDTO> JOBS = new HashMap<String, QuartzJobDTO>()
     {{
-        put(
-                "job_1",
-                QuartzJobDTO.builder()
-                        .name("v_sfn-to-t_sfn-job")
-                        .group("transfer-jobs")
-                        .description("V_SFN To T_SFN Transfer Job")
-                        .jobClass(Job1.class)
-                        .trigger(
-                                QuartzTriggerDTO.builder()
-                                        .name("v_sfn-to-t_sfn-trigger")
-                                        .group("transfer-triggers")
-                                        .cron("0 0 2 1/1 * ? *")
-                                        .build()
-                        )
-                        .build()
-        );
         put(
                 "job_1_plus",
                 QuartzJobDTO.builder()
