318c392209530a4e494d75ad545a44f807400bc4
OmerHalil <gamega55834@gmail.com>
2022-07-16 23:19:48 +0300

    add constructor to custom exception

--- src/main/java/com/quartz/quartzexample/excetion/JobFailedException.java
+++ src/main/java/com/quartz/quartzexample/excetion/JobFailedException.java
@@ -2,4 +2,8 @@ package com.quartz.quartzexample.excetion;
 
 public class JobFailedException extends RuntimeException
 {
+    public JobFailedException(String message)
+    {
+        super(message);
+    }
 }
