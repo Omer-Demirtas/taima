5e9e9bfc1ce58bb738b51b712b8eea22bf6648d1
OmerHalil <gamega55834@gmail.com>
2022-07-16 23:32:05 +0300

    Base Job Service updated for error handling

--- src/main/java/com/quartz/quartzexample/job/JobService.java
+++ src/main/java/com/quartz/quartzexample/job/JobService.java
@@ -1,8 +1,11 @@
 package com.quartz.quartzexample.job;
 
 import com.quartz.quartzexample.dto.QuartzJobDTO;
+import com.quartz.quartzexample.model.JobTracking;
+import com.quartz.quartzexample.utils.JobStatus;
 import com.quartz.quartzexample.utils.QuartzUtils;
 import lombok.extern.log4j.Log4j2;
+import org.apache.tomcat.jni.Local;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
@@ -16,19 +19,36 @@ public abstract class JobService extends QuartzJobBean
 {
     public QuartzJobDTO job;
 
-    public abstract void process();
+    public abstract String process();
 
     @Override
-    protected void executeInternal(JobExecutionContext context) throws JobExecutionException
+    protected void executeInternal(JobExecutionContext context)
     {
+        LocalDateTime startAt = LocalDateTime.now();
+        String description;
+        JobStatus jobStatus;
         String jobName = context.getJobDetail().getKey().getName();
         String jobGroup = context.getJobDetail().getKey().getGroup();
+
         job = QuartzUtils.getJobDetails(jobName, jobGroup);
 
-        log.info("Job start {} {} || at {} ", jobName, jobGroup , LocalDateTime.now());
+        log.info("Job start {} {} || at {} ", jobName, jobGroup , startAt);
 
-        process();
+        try
+        {
+            description = process();
+            jobStatus = JobStatus.SUCCESS;
+        }
+        catch (Exception ex)
+        {
+            description = ex.getMessage();
+            jobStatus = JobStatus.ERROR;
+            log.error("error at {} : {}", job.getName(), ex.getMessage());
+        }
 
-        log.info("Job finish at {}", LocalDateTime.now());
+        LocalDateTime finishAt = LocalDateTime.now();
+        JobTracking tracking = new JobTracking(job, jobStatus, description, startAt, finishAt);
+
+        log.info("Job finish at {}", finishAt);
     }
 }
