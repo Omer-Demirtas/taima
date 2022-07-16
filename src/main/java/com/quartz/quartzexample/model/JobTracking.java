3cdd25b97076bf15e29fb221a1b09579d07bdaea
OmerHalil <gamega55834@gmail.com>
2022-07-16 23:31:45 +0300

    Job Tracking model updated.

--- src/main/java/com/quartz/quartzexample/model/JobTracking.java
+++ src/main/java/com/quartz/quartzexample/model/JobTracking.java
@@ -1,11 +1,11 @@
 package com.quartz.quartzexample.model;
 
-import com.fasterxml.jackson.databind.DatabindException;
+import com.quartz.quartzexample.dto.QuartzJobDTO;
 import com.quartz.quartzexample.utils.JobStatus;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "QRTZ_JOB_TRACKING")
@@ -23,14 +23,17 @@ public class JobTracking
     @Column(name = "JOB_NAME", nullable = false)
     private String jobName;
 
+    @Column(name = "JOB_GROUP", nullable = false)
+    private String jobGroup;
+
     @Column(name = "TRIGGER_NAME")
     private String triggerName;
 
     @Column(name = "LAST_START_AT", nullable = false)
-    private Date lastStartAt;
+    private LocalDateTime lastStartAt;
 
     @Column(name = "LAST_FINISH_AT")
-    private Date lastFinishAt;
+    private LocalDateTime lastFinishAt;
 
     @Column(name = "DESCRIPTION")
     private String description;
@@ -38,4 +41,15 @@ public class JobTracking
     @Column(name = "JOB_STATUS")
     @Enumerated(EnumType.ORDINAL)
     private JobStatus jobStatus;
+
+    public JobTracking(QuartzJobDTO job, JobStatus jobStatus, String description, LocalDateTime lastStartAt, LocalDateTime lastFinishAt)
+    {
+        this.jobName = job.getName();
+        this.jobGroup = job.getGroup();
+        this.triggerName = job.getTrigger().getName();
+        this.jobStatus = jobStatus;
+        this.description = description;
+        this.lastStartAt = lastStartAt;
+        this.lastFinishAt = lastFinishAt;
+    }
 }
