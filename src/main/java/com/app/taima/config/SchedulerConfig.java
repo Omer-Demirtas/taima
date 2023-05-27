740e44c0fa7eb89b76ef26e0099361244636cef9
Ömer Demirtaş <47040453+Omer-Demirtas@users.noreply.github.com>
2023-05-27 21:28:13 +0300

    Scheduler Config updated

--- src/main/java/com/app/taima/config/SchedulerConfig.java
+++ src/main/java/com/app/taima/config/SchedulerConfig.java
@@ -2,13 +2,16 @@ package com.app.taima.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.util.Properties;
 
 @Configuration
@@ -19,23 +22,32 @@ public class SchedulerConfig {
 
 	private final ApplicationContext applicationContext;
 
-	private final QuartzProperties quartzProperties;
+	@Bean
+	public Properties quartzProperties() throws IOException {
+		PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+		propertiesFactoryBean.setLocation(new ClassPathResource("/quartz.properties"));
+		propertiesFactoryBean.afterPropertiesSet();
+		return propertiesFactoryBean.getObject();
+	}
 
 	@Bean
-	public SchedulerFactoryBean schedulerFactoryBean() {
+	public SchedulerFactoryBean schedulerFactoryBean() throws IOException {
 
 		SchedulerJobFactory jobFactory = new SchedulerJobFactory();
 		jobFactory.setApplicationContext(applicationContext);
 
-		Properties properties = new Properties();
-		properties.putAll(quartzProperties.getProperties());
+		//Properties properties = new Properties();
+		//properties.putAll(quartzProperties.getProperties());
 
 		SchedulerFactoryBean factory = new SchedulerFactoryBean();
 		factory.setOverwriteExistingJobs(true);
-		factory.setDataSource(dataSource);
-		factory.setQuartzProperties(properties);
 		factory.setJobFactory(jobFactory);
+
 		factory.setSchedulerName("taima");
+		Properties properties = quartzProperties();
+		factory.setQuartzProperties(properties);
+
+		factory.setDataSource(dataSource);
 		return factory;
 	}
 }
