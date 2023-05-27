e48e9fda736aa22616d34ead407fe94d7c86b102
Ömer Demirtaş <47040453+Omer-Demirtas@users.noreply.github.com>
2023-05-27 14:16:46 +0300

    Quartz Configuration added

---
+++ src/main/java/com/app/taima/config/SchedulerConfig.java
@@ -0,0 +1,41 @@
+package com.app.taima.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@RequiredArgsConstructor
+public class SchedulerConfig {
+
+	private final DataSource dataSource;
+
+	private final ApplicationContext applicationContext;
+
+	private final QuartzProperties quartzProperties;
+
+	@Bean
+	public SchedulerFactoryBean schedulerFactoryBean() {
+
+		SchedulerJobFactory jobFactory = new SchedulerJobFactory();
+		jobFactory.setApplicationContext(applicationContext);
+
+		Properties properties = new Properties();
+		properties.putAll(quartzProperties.getProperties());
+
+		SchedulerFactoryBean factory = new SchedulerFactoryBean();
+		factory.setOverwriteExistingJobs(true);
+		factory.setDataSource(dataSource);
+		factory.setQuartzProperties(properties);
+		factory.setJobFactory(jobFactory);
+		factory.setSchedulerName("TAIMA");
+		return factory;
+	}
+}
