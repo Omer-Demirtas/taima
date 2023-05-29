f2cac4bd3d8c84a97b5d3ac1503cfb1c0079d13b
Ömer Demirtaş <47040453+Omer-Demirtas@users.noreply.github.com>
2023-05-30 00:07:18 +0300

    config improvement

--- src/main/java/com/app/taima/config/SchedulerJobFactory.java
+++ src/main/java/com/app/taima/config/SchedulerJobFactory.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SchedulerJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
 
     private AutowireCapableBeanFactory beanFactory;
