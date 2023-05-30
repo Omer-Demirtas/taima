267ea43aab351a2c79fb774d0768938e67bb4ff6
Ömer Demirtaş <47040453+Omer-Demirtas@users.noreply.github.com>
2023-05-30 16:28:16 +0300

    Job Service updated

--- src/main/java/com/app/taima/service/impl/JobServiceImpl.java
+++ src/main/java/com/app/taima/service/impl/JobServiceImpl.java
@@ -1,21 +1,38 @@
 package com.app.taima.service.impl;
 
+import com.app.taima.Entity.Process;
 import com.app.taima.Entity.SchedulerJob;
+import com.app.taima.enums.ProcessType;
 import com.app.taima.repository.SchedulerRepository;
 import com.app.taima.service.JobService;
+import com.app.taima.service.RestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Log4j2
 @Service
 @RequiredArgsConstructor
 public class JobServiceImpl implements JobService {
     private final SchedulerRepository schedulerRepository;
+    private final RestService restService;
+
+    private void executeRestRequest() {
+
+    }
 
     @Override
     public void execute(String name, String group) {
+        log.info("name group {} {} ", name, group);
         SchedulerJob schedulerJob = schedulerRepository.findByNameAndGroup(name, group);
         log.info(schedulerJob);
+        for (Process process : schedulerJob.getProcesses()) {
+            log.info("pt {} ", process.getProcessType());
+            if (process.getProcessType() == ProcessType.RestRequest) {
+                String result = restService.get(process.getUrl());
+                log.info("Result: {}", result);
+            }
+        }
     }
 }
