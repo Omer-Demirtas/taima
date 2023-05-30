02200f6205cb1cf59b30402df38b2bca11ab2b86
Ömer Demirtaş <47040453+Omer-Demirtas@users.noreply.github.com>
2023-05-30 15:33:13 +0300

    Job service & job updated

--- src/main/java/com/app/taima/job/SampleCronJob.java
+++ src/main/java/com/app/taima/job/SampleCronJob.java
@@ -1,12 +1,9 @@
 package com.app.taima.job;
 
-import com.app.taima.repository.SchedulerRepository;
 import com.app.taima.service.JobService;
-import com.app.taima.service.SchedulerService;
 import lombok.extern.log4j.Log4j2;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
