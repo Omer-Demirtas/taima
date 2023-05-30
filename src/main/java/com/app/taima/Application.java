6e786ad263b0a2add2b6b5ff6a320d9e4543c634
Ömer Demirtaş <47040453+Omer-Demirtas@users.noreply.github.com>
2023-05-30 16:27:29 +0300

    Rest Service added

--- src/main/java/com/app/taima/Application.java
+++ src/main/java/com/app/taima/Application.java
@@ -5,6 +5,7 @@ import org.modelmapper.TypeMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
@@ -19,4 +20,10 @@ public class Application {
 
         return modelMapper;
     }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }
